19c64bb7cd9f3b27ee8535474043a5a5bd7b8dc2
gabrielvictalino <gabrielvictalino@gmail.com>
2024-11-27 17:53:25 -0300

    criei um arquivo.css para a pagina Arduino e outros bugfixes

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.nome').addEventListener('mouseover', () => {
-    const title = document.querySelector('.name-effect');
-    title.classList.add('active');
-});
-
-document.querySelector('.nome').addEventListener('mouseout', () => {
-    const title = document.querySelector('.name-effect');
-    title.classList.remove('active');
-});
